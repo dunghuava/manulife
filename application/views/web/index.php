677b42201df18f71ea84484a094b560f1f818d65
TruongThuan1997 <34022612+TruongThuan1997@users.noreply.github.com>
2020-11-05 22:11:56 +0700

    fix index

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -30,6 +30,9 @@
 
 <?php include('sec-muctieu.php') ?>
 
+<?php
+    $list_product = $this->Web_M->q("select * from db_product a inner join db_category b on a.product_category_id=b.cate_id order by rand() limit 3");
+?>
 <section class="sec-product">
     <div class="container full-w"><br>
         <div class="row">
@@ -41,15 +44,15 @@
             </div>
             <div class="col-md-9">
                 <div class="row">
-                    <?php for ($i=0;$i<3;$i++){ ?>
+                    <?php foreach ($list_product as $key => $item) { ?>
                     <div class="col-md-4">
                             <div class="item-product">
-                                <p>Bảo hiểm trọn đời <a style="float:right" href="" class="readmore"><span class="fa fa-angle-right"></span>Xem thêm</a></p>  
-                                <h3>Hành trình hạnh phúc</h3>
+                                <p><?=$item['cate_title']?> <a style="float:right" href="" class="readmore"><span class="fa fa-angle-right"></span>Xem thêm</a></p>  
+                                <h3><?=$item['product_title']?></h3>
                                 <br>
                                 <img src="<?=img_path('se_05.svg')?>" alt="">
                                 <br><br>
-                                <div class="des">Xây dựng kế hoạch tài chính trọn đời, linh hoạt bảo vệ trên mọi hành trình của cuộc sống</div>
+                                <div class="des"><?=$item['product_description']?></div>
                             </div>
                     </div>
                     <?php } ?>
@@ -60,13 +63,13 @@
 </section>
 <?php 
     // thay where post_type=2 de lay dich vu
-    $dichvu = $this->Web_M->q("select a.post_id,a.post_title,b.cate_title,a.post_img,a.post_alias from db_post a inner join db_category b on a.post_category_id=b.cate_id order by rand() limit 6");
+    $list_post = $this->Web_M->q("select a.post_id,a.post_title,b.cate_title,a.post_img,a.post_alias from db_post a inner join db_category b on a.post_category_id=b.cate_id where a.post_type=1 order by rand() limit 6");
 ?>
 <section class="sec-product">
     <div class="container full-w"><br>
         <div class="row">
             <div class="col-md-3">
-                <h3 class="product-title">Manulife Việt Nam</h3><br>
+                <h3 class="product-title">Các bài viết</h3><br>
                 <a href="" class="readmore">
                     <span class="fa fa-angle-right"></span>Xem thêm
                 </a>
@@ -74,14 +77,14 @@
             <div class="col-md-9">
                 <div class="row">
                     <?php
-                    foreach ($dichvu as $item){ 
+                    foreach ($list_post as $item){ 
                         $styles=array(
                             0=>"background:#00A758 url(".img_path($item['post_img']).") no-repeat",
                             1=>"background-size:cover",
                             2=>"background-position-x:center"
                         );
                     ?>
-                    <div class="col-md-4">
+                        <div class="col-md-4">
                                 <div class="item-product has-bg" style="<?=implode(';',$styles)?>">
                                     <div class="title">
                                         <p><?=$item['cate_title']?> <a style="float:right" href="<?=base_url('post/'.$item['post_alias'].'-'.$item['post_id'])?>.html" class="readmore"><span class="fa fa-angle-right"></span>Xem thêm</a></p>  
@@ -95,7 +98,7 @@
                                     <div class="overlay"></div>
                                     <div class="des"><?=$item['post_description']?></div>
                                 </div>
-                    </div>
+                        </div>
                     <?php } ?>
                 </div>
             </div>
@@ -119,8 +122,8 @@
                     <?php foreach ($dichvu as $item){ ?>
                     <div class="col-md-4">
                         <a href="<?=base_url($item['cate_alias'])?>.html">
-                            <div class="item-product" style="display:flex">
-                                <img src="<?=img_path('se_05.svg')?>" alt="">
+                            <div class="item-product" style="display:flex;height: 115px;overflow: hidden;">
+                                <img src="<?=img_path('se_05.svg')?>" alt="" style="margin-right: 20px;">
                                 <div class="des"><?=$item['cate_title']?></div>
                             </div>
                         </a>
